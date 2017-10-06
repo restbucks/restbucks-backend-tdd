62c004c37c802ac52dca05df1faae2d50767561d
hippoom <hippoom@gmail.com>
2017-10-06 21:41:20 +0800

    Fix @DataJpaTest

--- webmvc-test/src/test/groovy/org/restbucks/tdd/jpa/AbstractJpaTest.groovy
+++ webmvc-test/src/test/groovy/org/restbucks/tdd/jpa/AbstractJpaTest.groovy
@@ -2,19 +2,24 @@ package org.restbucks.tdd.jpa
 
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
 
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
 
 @RunWith(SpringRunner)
-//@DataJpaTest(showSql = true)
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-//@Import(JpaConfiguration)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@DataJpaTest(showSql = true)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(JpaConfiguration)
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @ActiveProfiles("test")
 //@Commit
 abstract class AbstractJpaTest {
